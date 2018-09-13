2c0dce038eaa88e8a0aca3d2eea79d8e4c5c05b2
hexxcointakeover <westqoo@mail.com>
2018-09-13 22:46:07 +0300

    more bzconnections

--- src/bznode-sync.h
+++ src/bznode-sync.h
@@ -18,7 +18,7 @@ static const int BZNODE_SYNC_MNW             = 3;
 static const int BZNODE_SYNC_FINISHED        = 999;
 static const int BZNODE_SYNC_TICK_SECONDS    = 6;
 static const int BZNODE_SYNC_TIMEOUT_SECONDS = 30; // our blocks are 2.5 minutes so 30 seconds should be fine
-static const int BZNODE_SYNC_ENOUGH_PEERS    = 2;
+static const int BZNODE_SYNC_ENOUGH_PEERS    = 5;
 extern CBznodeSync bznodeSync;
 
 //
